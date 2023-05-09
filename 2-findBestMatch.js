a71ae300ca966774c44b2cce4605c6a29fe2af28
Constantin Danilov <Constantin.Danilov@endava.com>
2023-05-09 14:27:24 +0300

    2-findbestmatch task done

--- 2-findBestMatch.js
+++ 2-findBestMatch.js
@@ -1,26 +1,8 @@
-const findBestMatch = (k, arr) => {
-    
-    let newArr = []
-    // arr.forEach(num => {
-        
-        
-    //     newArr.push(k > 0 ? k - num : k + num)
-    // });
-    
-    // return newArr.sort((a, b) => a - b)[0]
-    // return arr.reduce((acc, currVal) => {
-    //     // console.log('acc', acc);
-    //     // console.log('currVal', currVal);
-    //     let newNum = acc - currVal < acc ? acc - currVal : acc
-    //     return newNum
-    // }, 0)
+const findBestMatch = (k, arr) =>
+  arr.reduce((prev, curr) =>
+    Math.abs(curr - k) < Math.abs(prev - k) ? curr : prev
+  );
 
-    arr.forEach(num => newArr.push(num-k))
-    return newArr.sort((a, b) => b - a)
-}
+const arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
-const arr = [10, 50, 200, 340, 1, ];
-
-console.log(findBestMatch(60, arr));
-
-// console.log(-96-100)
+console.log(findBestMatch(26, arr));
